e62c06c37fa60e96c86d03b18a445b3a36fc0eb2
devlinpadayachee@gmail.com <devlinpadayachee@gmail.com>
2017-06-27 00:18:23 +0200

    6/27/2017

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-'use strict';
+var Bot = require('slackbots');
+var sqlite3 = require('sqlite3').verbose();
 
-const Hapi = require('hapi');
+//const { urls, domain, slashtags } = commandParser(body.text)
+// create a bot
+var settings = {
+    token: '',
+    name: 'Sparky',
+    as_user : true
+};
 
-// Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+var bot = new Bot(settings);
+
+bot.on('start', function() {
+    bot.postMessageToChannel('bot_testing', 'Hello channel!');
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
-
-        return reply('hello world');
-    }
+bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm 
+    console.log(data);
 });
 
-// Start the server
-server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+var db = new sqlite3.Database('sparky.db');
+var check;
+db.serialize(function() {
+
+  db.run("CREATE TABLE if not exists user_info (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+
 });
+
+db.close();
